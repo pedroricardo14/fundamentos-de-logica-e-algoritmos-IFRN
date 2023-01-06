8d184a4c23b5cd41e34a9311b65bef65e26c785d
Pedro Ricardo <115710304+pedroricardo14@users.noreply.github.com>
2023-01-06 16:11:47 -0300

    Create questao4.py

---
+++ lista1/questao4.py
@@ -0,0 +1,10 @@
+"""4) Elaborar um programa que calcule e apresente o volume de uma caixa retangular, por meio da fórmula
+VOLUME ← COMPRIMENTO * LARGURA * ALTURA."""
+
+comprimento = float(input("Comprimento? "))
+largura = float(input("Largura? "))
+altura = float(input("Altura?" ))
+
+volume = comprimento * largura * altura
+
+print(f"O volume é {volume} m².")
