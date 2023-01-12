f3fdedc68e6773813a3a44c2dfbf390156266b1f
Pedro Ricardo <115710304+pedroricardo14@users.noreply.github.com>
2023-01-12 18:45:32 -0300

    Create questao7.py

---
+++ lista1/questao7.py
@@ -0,0 +1,8 @@
+"""7) Digite as notas de um aluno para os 4 bimestres do ano e calcule a sua média aritmética."""
+
+n1 = float(input("Nota1: "))
+n2 = float(input("Nota2: "))
+n3 = float(input("Nota3: "))
+n4 = float(input("Nota4: "))
+media = (n1+n2+n3+n4)/4
+print(f"Média: {media:.1f}")
