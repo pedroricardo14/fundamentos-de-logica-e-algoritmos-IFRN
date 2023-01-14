613dc8610a28f026f40d8badd37d96d7b5bd1e69
Pedro Ricardo <115710304+pedroricardo14@users.noreply.github.com>
2023-01-14 15:55:49 -0300

    Create desvios_multiplos(temperatura).py

---
+++ lista1/desvios_multiplos(temperatura).py
@@ -0,0 +1,11 @@
+temp = float(input("Digite a temperatura: "))
+
+if temp > 18 :
+  print("Que calor!")
+
+  if temp < 25 : 
+    print("Que frio!")
+
+if temp >= 18 :
+  if temp <= 25 :
+    print("Clima Agradável")
