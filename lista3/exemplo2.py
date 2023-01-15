b8733ea416d51afb2079e6e4350f0505cd0ac140
Pedro Ricardo <115710304+pedroricardo14@users.noreply.github.com>
2023-01-15 16:27:34 -0300

    Create exemplo2.py

---
+++ lista3/exemplo2.py
@@ -0,0 +1,8 @@
+# O caminhão só pode viajar com carga entra 500 e 1000 kg
+
+peso = float(input("Peso? "))
+if peso >= 500 and peso <= 1000 :
+  print("Viagem liberada.")
+else :
+  print("Revisar a carga.")
+  
