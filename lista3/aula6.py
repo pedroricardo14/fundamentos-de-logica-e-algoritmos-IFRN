1bedc112ecaa3015925253d45b66001cc11df91b
Pedro Ricardo <115710304+pedroricardo14@users.noreply.github.com>
2023-01-15 16:43:02 -0300

    Create aula6.py

---
+++ lista3/aula6.py
@@ -0,0 +1,8 @@
+# pergunta o dia da semana e informa se tem aula nesse dia.
+dia = input("Qual o dia da semana? ")
+
+if dia == "segunda" or dia == "quarta" or dia == "sexta" :
+  print("Nesse dia TEM aula.")
+else : 
+  print("NÃO tem aula nesse dia.")
+  
