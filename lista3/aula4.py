2fb3f6541b35e24e5c2275898aa4aff480e08c31
Pedro Ricardo <115710304+pedroricardo14@users.noreply.github.com>
2023-01-15 16:35:45 -0300

    Create aula4.py

---
+++ lista3/aula4.py
@@ -0,0 +1,8 @@
+numero = int(input("Número do candidato? "))
+
+if numero == 51 or numero == 33 :
+  print("O candidato está cadastrado.")
+else :
+  print("Esse candidato não existe.")
+
+#O "or" é parecido com o and. A diferença é que ele funciona mesmo quando a resposta de um é não e a do outro é sim.
