3f66ccd57cfcaf604e38e1f6eb08b70fc9054364
Pedro Ricardo <115710304+pedroricardo14@users.noreply.github.com>
2023-01-16 09:24:54 -0300

    Create aula7.py

---
+++ lista3/aula7.py
@@ -0,0 +1,6 @@
+temp = float(input("Temperatura? "))
+
+if (temp>=0 and temp<=20) or (temp>=80 and temp<=100) :
+  print("Faixa de operação aceita.")
+else : 
+  print("PERIGO!!! Temperatura inadequada.")
