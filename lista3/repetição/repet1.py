82d544cde232e46294f26099ed69afc3ccc9fc12
Pedro Ricardo <115710304+pedroricardo14@users.noreply.github.com>
2023-01-16 19:27:38 -0300

    Create repet1.py

---
+++ lista3/repetição/repet1.py
@@ -0,0 +1,10 @@
+resposta = "não" #valor inicial
+
+# teste lógico
+while resposta == "não" :
+
+  #incremento - mudança na variável
+  resposta = input("A gente já chegou?")
+
+print("Até que enfim!")
+  
