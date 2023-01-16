6f1e56d9b45650132062e3a387cf8f1e09e40547
Pedro Ricardo <115710304+pedroricardo14@users.noreply.github.com>
2023-01-16 09:31:39 -0300

    Create aula9.py

---
+++ lista3/aula9.py
@@ -0,0 +1,12 @@
+v1 = int(input("Valor1: "))
+v2 = int(input("Valor2: "))
+op = input("Qual a operação (+, -, *, /)? ")
+
+if op == "+" :
+  print(v1+v2)
+elif op == "-" :
+  print(v1-v2)
+elif op == "*" :
+  print(v1*v2)
+elif op == "/" :
+  print(v1/v2)
