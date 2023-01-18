abc8fb0f397711305b4236c742d86b6d9560b551
Pedro Ricardo <115710304+pedroricardo14@users.noreply.github.com>
2023-01-18 20:01:03 -0300

    Create comandolen.py

---
+++ lista3/listas/comandolen.py
@@ -0,0 +1,9 @@
+frutas = ["uva","caju","laranja","maçã","pêra","banana","maracujá","acerola","graviola"]
+
+print(f"A lista tem {len(frutas)} itens.")
+
+posicao = 0
+while posicao < len(frutas) :
+  print(f"Você gosta de {frutas[posicao]}?")
+  posicao = posicao + 1
+  
