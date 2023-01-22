aa4f191e569026823561572b81cd32d0f8fdebd1
Pedro Ricardo <115710304+pedroricardo14@users.noreply.github.com>
2023-01-22 17:35:22 -0300

    Create exlista2.py

---
+++ lista5/exlista2.py
@@ -0,0 +1,9 @@
+brinquedos = ["bola", "boneca", "carinho"]
+          #0      #1          #2
+notas = [7.4, 8.4, 9.8, 10.0]
+
+cliente = ["Joaquim", 430, "(84) 98765-4321"]
+
+brinquedos.append("patinete")    #A função "append" significa "acrescente"
+brinquedos.append("damas")
+print(brinquedos)
