6cdf064fe19e39cde5a561db4a7fb3a7daed1d8a
Pedro Ricardo <115710304+pedroricardo14@users.noreply.github.com>
2023-01-26 19:27:31 -0300

    Create exlista3.py

---
+++ lista5/exlista3.py
@@ -0,0 +1,9 @@
+# Pesquisa em uma lista. 
+frutas = ["laranja", "banana", "manga", "uva", "melancia", "pêra", "caju", "pitaia"]
+
+pesquisa = input("Qual fruta você gosta? ")
+
+if pesquisa in frutas :
+  print("Eu vendo essa fruta. Quer comprar? ")
+else :
+  print("Essa fruta eu não tenho.")
