62546042d8aa5431c74bc8f32f9e89942c38818c
Pedro Ricardo <115710304+pedroricardo14@users.noreply.github.com>
2023-01-26 19:31:08 -0300

    Create exlista4.py

---
+++ lista5/exlista4.py
@@ -0,0 +1,8 @@
+# Listas aninhadas
+fruta1 = ["laranja", 10, 0.35]
+carne1 = ["maminha", 1.5, 45.00]
+cereal1 = ["feijão", 2, 6.30]
+
+compras = [fruta1, carne1, cereal1]
+              #0    #1        #2
+print(compras[1][2])        #Se colocar um só colchete, o python vai imprimir a lista inteira. Já se colocar mais de dois parênteses, o primeiro é do intem da lista e a patir do segundo é subitem.
