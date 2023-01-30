3f0bf32e66ea3b10e72dd755a7ba2241911cf8fe
Pedro Ricardo <115710304+pedroricardo14@users.noreply.github.com>
2023-01-30 12:32:39 -0300

    Create exlista8.py

---
+++ lista5/exlista8.py
@@ -0,0 +1,7 @@
+alunos = ["abel", "bianca", "celino", "diana", "elias", "fernanda", "gabriel", "helenice"]
+print(alunos)
+
+alunos.pop(0)    #del(alunos[0])
+alunos.pop(2)    #del(alunos[2])
+alunos.pop(4)    #del(alunos[4])
+print(alunos)
