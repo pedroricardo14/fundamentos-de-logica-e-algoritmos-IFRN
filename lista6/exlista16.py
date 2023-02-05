61881c7d9ba5ecba84db14f2814e57cd03d16274
Pedro Ricardo <115710304+pedroricardo14@users.noreply.github.com>
2023-02-05 13:14:25 -0300

    Create exlista16.py

---
+++ lista6/exlista16.py
@@ -0,0 +1,7 @@
+lista = [15, 29, 77, 4, 16, 81, 34]
+
+# obter o maior valor de uma lista
+print("Maior valor =", max(lista))
+
+# obter o menor valor de uma lista
+print("Menor valor=", min(lista))
