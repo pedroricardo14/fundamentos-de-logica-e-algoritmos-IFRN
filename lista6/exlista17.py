2777d7b7444043600f438164f23f1090dd955409
Pedro Ricardo <115710304+pedroricardo14@users.noreply.github.com>
2023-02-05 13:15:19 -0300

    Create exlista17.py

---
+++ lista6/exlista17.py
@@ -0,0 +1,7 @@
+# contar o número de ocorrências de um valor
+
+produtos = ["bolo", "suco", "salgado", "bolo", "picolé", "bolo", "pastel", "refrigerante", "suco"]
+
+print(f"Bolos vendidos = {produtos.count('bolo')}")
+print(f"Sucos vendidos = {produtos.count('suco')}")
+print(f"Salgados vendidos = {produtos.count('salgado')}")
