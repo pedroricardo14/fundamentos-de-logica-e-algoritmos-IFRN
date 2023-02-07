f0985806e7febf11349d078399a1e2b07229cc9d
Pedro Ricardo <115710304+pedroricardo14@users.noreply.github.com>
2023-02-07 19:59:46 -0300

    Create questao1.py

---
+++ 2° Período - Fundamentos de lógica e Algoritmos/range/questao1.py
@@ -0,0 +1,31 @@
+# 1 - Crie uma sequência de 0 até 14. 
+"""
+print(list(range(15)))"""
+
+# 2 - Crie uma sequência de 0 até 30. 
+
+"""print(list(range(30+1)))""" #ou 31
+
+# 3 - Crie uma sequência de 1 até 25. 
+
+"""print(list(range(1, 26)))"""
+
+# 4 - Crie uma sequência de 5 até 49. 
+
+"""print(list(range(5, 50)))"""
+
+# 5 - Crie uma sequência com os números pares de 2 até 16. 
+
+"""print(list(range(2, 17, 2)))"""
+
+# 6 - Impares de 7 até 27. 
+
+"""print(list(range(7, 27 + 1, 2)))  #ou 28"""
+
+# 7 - 
+
+"""print(list(range(18, 31, 3)))"""
+
+# 8 - 
+
+"""print(list(range(35, 80, 11)))"""
