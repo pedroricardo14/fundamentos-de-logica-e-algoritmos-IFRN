2d39b658dd6c43076a4fc08b4629e3967b81fa3b
Pedro Ricardo <115710304+pedroricardo14@users.noreply.github.com>
2023-02-07 20:05:10 -0300

    Create range1.py

---
+++ 2° Período - Fundamentos de lógica e Algoritmos/range/range1.py
@@ -0,0 +1,10 @@
+"""numeros = list(range(20))    #range de outra forma
+print(numeros)"""
+
+"""numeros = list(range(1, 21))     # da pra fazer usando 21 + 1  #pra ir de 1 até 20
+print(numeros)"""
+
+# Aumentando o valor de 4 em 4(funciona com qualquer numeração):
+
+numeros = list(range(0, 21, 4))
+print(numeros)
